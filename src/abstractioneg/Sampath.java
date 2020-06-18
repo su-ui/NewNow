ed2cbd82b63fb16c343ff24724d5d1e78e666875
Su <ssvagle@gmail.com>
2020-06-18 13:25:25 +0530

    added new class

---
+++ src/abstractioneg/Sampath.java
@@ -0,0 +1,12 @@
+package abstractioneg;
+
+public class Sampath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Newly added");
+
+	}
+
+}
